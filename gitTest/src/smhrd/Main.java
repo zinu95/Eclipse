222a248ade3083c1d3d5e2c198416fb498c6df5b
Jinu <Jinu@121.179.5.230>
2021-06-17 00:42:38 +0900

    hello world 메세지 추가

--- gitTest/src/smhrd/Main.java
+++ gitTest/src/smhrd/Main.java
@@ -3,7 +3,8 @@ package smhrd;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
+		System.out.println("Hello world!!");
 
 	}
 
